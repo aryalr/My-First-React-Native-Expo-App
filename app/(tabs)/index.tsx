643c953ba12c32fce255430380d1470a4e7d5817
aryalr <aryajati@gmail.com>
2025-01-29 21:10:50 +0700

    Fix the error with image source typo and added the button and allert when pressing a button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
 
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
-const PlaceholderImage = require('@/assets/images/background-Image.png');
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Chose a photo"/>
+        <Button label="Use this photo"/>
       </View>
     </View>
   );
@@ -32,4 +37,8 @@ const styles = StyleSheet.create({
   imageContainer : {
     flex: 1,
   },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  }
 });
