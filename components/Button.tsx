bc854e783a44922dd60006b48ab9047017550dd4
aryalr <aryajati@gmail.com>
2025-01-31 12:24:46 +0700

    ,

--- components/Button.tsx
+++ components/Button.tsx
@@ -20,7 +20,7 @@ export default function Button({label, theme} : Props) {
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
                     <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
                 </Pressable>
-                </View>
+            </View>
         )
     }
 
